fa58b105d804f28f0313956d11465a8fc23bbb42
Swopnab <89373272+Swopnab@users.noreply.github.com>
2025-03-24 03:18:57 -0500

    BrainRot #2

--- quiz.java
+++ quiz.java
@@ -1,33 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Quiz extends JFrame{
 
-    String questions[][] = new String[25][5];
+public class Quiz extends JFrame implements ActionListener { 
+
+    String questions[][] = new String[25][1];
     String answers[][] = new String[25][5];
+    String useranswers[][] = new String[25][1];
 
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+
+    ButtonGroup groupoptions;
+    JButton next, submit;
+
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int score = 0;
+    public static int count = 0;
+
+    String name;
 
 
     Quiz() {
         setBounds(50, 0, 1400, 900);
-        getContentPane().setBackground(Color.white);
+        getContentPane().setBackground(Color.black);
         setLayout(null);
 
         ImageIcon i1= new ImageIcon("DE_QuizTemplate.jpg");
         Image img = i1.getImage().getScaledInstance(580, 900, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(img);
         JLabel image = new JLabel(scaledIcon);
-        image.setBounds(0, 0, 580, 900);
+        image.setBounds(0, 0, 350, 900);
         add(image);
         setVisible(true);
 
 
-        JLabel qno = new JLabel("1. ");
+        qno = new JLabel();
         qno.setBounds(750, 200, 500, 50);
         qno.setFont(new Font("SansSerif", Font.BOLD,24));
         add(qno);
 
-        JLabel question = new JLabel("Questions");
+        question = new JLabel();
         question.setBounds(800, 200, 500, 50);
         question.setFont(new Font("SansSerif", Font.BOLD,36));
         add(question);
@@ -64,10 +79,10 @@ public class Quiz extends JFrame{
                 questions[4][4] = "Carlo Ancelotti";
         
                 questions[5][0] = "What are Atletico Madrid's traditional home colors?";
-                questions[5][1] = "Red and white stripes";
-                questions[5][2] = "Blue and white";
+                questions[5][1] = "Red and black stripes";
+                questions[5][2] = "Blue and black";
                 questions[5][3] = "Yellow and red";
-                questions[5][4] = "Black and white";
+                questions[5][4] = "Black and black";
         
                 questions[6][0] = "Which team did Atletico Madrid defeat in the 2018 UEFA Europa League final?";
                 questions[6][1] = "Liverpool";
@@ -128,7 +143,7 @@ public class Quiz extends JFrame{
                 answers[2][0] = "Luis Aragones";
                 answers[3][0] = "11";
                 answers[4][0] = "Diego Simeone";
-                answers[5][0] = "Red and white stripes";
+                answers[5][0] = "Red and black stripes";
                 answers[6][0] = "Marseille";
                 answers[7][0] = "Madrid";
                 answers[8][0] = "Real Madrid";
@@ -140,48 +155,186 @@ public class Quiz extends JFrame{
                 answers[14][0] = "Fulham";
 
 
-        JRadioButton opt1 = new JRadioButton("Option 1");
+        opt1 = new JRadioButton();
         opt1.setBounds(900, 300, 500, 80);
         opt1.setBackground(Color.white);
         opt1.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(opt1);
 
-        JRadioButton opt2 = new JRadioButton("Option 2");
+        opt2 = new JRadioButton();
         opt2.setBounds(900, 400, 500, 80);
         opt2.setBackground(Color.white);
         opt2.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(opt2);
 
-        JRadioButton opt3 = new JRadioButton("Option 3");
+        opt3 = new JRadioButton();
         opt3.setBounds(900, 500, 500, 80);
         opt3.setBackground(Color.white);
         opt3.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(opt3);
 
-        JRadioButton opt4 = new JRadioButton("Option 4");
+        opt4 = new JRadioButton();
         opt4.setBounds(900, 600, 500, 80);
         opt4.setBackground(Color.white);
         opt4.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(opt4);
 
 
-        ButtonGroup grouppoptions = new ButtonGroup();
-        grouppoptions.add(opt1);
-        grouppoptions.add(opt2);
-        grouppoptions.add(opt3);
-        grouppoptions.add(opt4);
+        ButtonGroup groupoptions = new ButtonGroup();
+        groupoptions.add(opt1);
+        groupoptions.add(opt2);
+        groupoptions.add(opt3);
+        groupoptions.add(opt4);
 
-        Button next = new Button("Next");
-        next.setBounds(1100, 700, 200, 80);
+        next = new JButton("Next");
+        next.setBounds(900, 500, 500, 50);
+        next.setFont(new Font("Dialog", Font.PLAIN, 24));
+        next.setBackground(new Color(30, 144, 255));
+        next.setForeground(Color.black);
+        next.addActionListener(this);
         add(next);
+
+        submit = new JButton("Submit");
+        submit.setBounds(900, 500, 500, 50);
+        submit.setFont(new Font("Dialog", Font.PLAIN, 24));
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.black);
+        submit.addActionListener(this);
+        submit.setEnabled(false);
+        add(submit);
+
+        start(count);
 
         setVisible(true);
 
     }
 
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+               useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            
+            count++;
+            start(count);
+        } else if (ae.getSource() == submit) {
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1])) {
+                    score += 10;
+                } else {
+                    score += 0;
+                }
+            }
+            setVisible(false);
+            new Score(name, score);
+        }
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        String time = "Time left - " + timer + " seconds"; // 15
+        g.setColor(Color.RED);
+        g.setFont(new Font("Dialog", Font.PLAIN, 30));
+        
+        if (timer > 0) { 
+            g.drawString(time, 800 , 100);
+        } else {
+            g.drawString("Times up!!", 800 , 100);
+        }
+        
+        timer--; // 14
+        
+        try {
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1])) {
+                        score += 10;
+                    } else {
+                        score += 0;
+                    }
+                }
+                setVisible(false);
+                new Score(name, score);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++; // 0 // 1
+                start(count);
+            }
+        }
+        
+    }
+    
+    public void start(int count) {
+        qno.setText("" + (count + 1) + ". ");
+        question.setText(questions[count][0]);
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+        
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+        
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+        
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupoptions.clearSelection();
+    }
+    
     public static void main(String[] args) {
         new Quiz();
     }
-    
-    
 }
